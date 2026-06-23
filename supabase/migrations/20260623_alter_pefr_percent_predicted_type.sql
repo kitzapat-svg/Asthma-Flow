595e0c2f8316e078e1658cb159c84eee93b9e3ca
kitzapat-svg <kitzapat@gmail.com>
2026-06-23 09:28:00 +0700

    chore: remove temporary migration file

--- supabase/migrations/20260623_alter_pefr_percent_predicted_type.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Alter pefr_percent_predicted column type in visits table to REAL
--- Date: 2026-06-23
-
-ALTER TABLE visits 
-  ALTER COLUMN pefr_percent_predicted TYPE REAL USING pefr_percent_predicted::REAL;
